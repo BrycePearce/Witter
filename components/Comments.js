4d36f2060d30ba1573de4ebf847468156d1a5fee
BrycePearce <bpearce@angelo.edu>
2017-06-02 21:52:11 -0500

    added comments, and ability to add/remove comments

--- components/Comments.js
+++ components/Comments.js
@@ -1,10 +1,39 @@
 import React from 'react';
-
 const Comments = React.createClass({
+
+  renderComment(comment, i) {
+    return (
+      <div className="comment" key={i}>
+        <p>
+          <strong>{comment.user}</strong>
+          {comment.text}
+          <button className="remove-comment" onClick={this.props.removeComment.bind(null, this.props.params.postId, i)} >&times;</button>
+        </p>
+      </div>
+    )
+  },
+
+  handleSubmit(e) {
+    //prevent from refreshing
+    e.preventDefault();
+    console.log(this.refs);
+    const postId = this.props.params.postId;
+    const author = this.refs.author.value;
+    const comment = this.refs.comment.value;
+    //pass our values to our actionCreator to post the data
+    this.props.addComment(postId, author, comment);
+    //erase the form after we submit data
+    this.refs.commentForm.reset();
+},
   render() {
     return (
-      <div className="comment">
-        I'm the comments!!!!
+      <div className="comments">
+        {this.props.postComments.map(this.renderComment)}
+        <form ref="commentForm" className="comment-form" onSubmit={this.handleSubmit}>
+          <input type="text" ref="author" placeholder="author" />
+          <input type="text" ref="comment" placeholder="comment" />
+          <input type="submit" hidden />
+        </form>
       </div>
     )
   }
--- components/Single.js
+++ components/Single.js
@@ -7,12 +7,14 @@ const Single = React.createClass({
     // index of the post
     const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
     const post = this.props.posts[i];
+    //get the comments, even if there are no comments
+    const postComments = this.props.comments[this.props.params.postId] || [];
     // which will get us the post
 
     return (
       <div className="single-photo">
         <Photo i={i} post={post}{...this.props} />
-        <Comments />
+        <Comments postComments={postComments} {...this.props} />
 
       </div>
     )
